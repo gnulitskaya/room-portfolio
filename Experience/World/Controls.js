b61540bf61f362f2d0b6a598cbe2baa5672c15f6
gnulitskaya <webgnule@mail.ru>
2023-03-05 18:07:25 +0300

    add gsap

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import Experience from "../Experience.js";
 import GSAP from "gsap";
+import Experience from "../Experience.js";
 
 export default class Controls {
     constructor() {
@@ -10,16 +10,21 @@ export default class Controls {
         this.time = this.experience.time;
         this.camera = this.experience.camera;
 
-        this.progress = 0;
-        this.dummyCurve = new THREE.Vector3(0, 0, 0);
+        // this.progress = 0;
+        // this.dummyCurve = new THREE.Vector3(0, 0, 0);
 
         this.lerp = {
             current: 0,
             target: 1,
-            ease: 0.1
+            ease: 0.1,
         }
 
         this.position = new THREE.Vector3(0, 0, 0);
+        this.lookAtPosition = new THREE.Vector3(0, 0, 0);
+
+        this.directionalVector = new THREE.Vector3(0, 0, 0);
+        this.staticVector = new THREE.Vector3(0, 1, 0);
+        this.crossVector = new THREE.Vector3(0, 0, 0);
 
         this.setPath();
         this.onWheel();
@@ -29,11 +34,10 @@ export default class Controls {
     setPath() {
         // create a closed wavey loop
         this.curve = new THREE.CatmullRomCurve3( [
-            new THREE.Vector3( -10, 0, 10 ),
-            new THREE.Vector3( -5, 5, 5 ),
-            new THREE.Vector3( 0, 0, 0 ),
-            new THREE.Vector3( 5, -5, 5 ),
-            new THREE.Vector3( 10, 0, 10 )
+            new THREE.Vector3( -5, 0, 0 ),
+            new THREE.Vector3( 0, 0, -5 ),
+            new THREE.Vector3( 5, 0, 0 ),
+            new THREE.Vector3( 0, 0, 5 )
         ], true );
 
         const points = this.curve.getPoints( 50 );
@@ -49,12 +53,11 @@ export default class Controls {
     onWheel() {
         window.addEventListener("wheel", (e) => {
             if (e.deltaY > 0) {
-                this.lerp.target += 0.1;
+                this.lerp.target += 0.01;
+                this.back = false;
             } else {
-                this.lerp.target -= 0.1;
-                // if(this.progress < 0) {
-                //     this.progress = 1;
-                // }
+                this.lerp.target -= 0.01;
+                this.back = true;
             }
         })
     }
@@ -64,17 +67,36 @@ export default class Controls {
     }
 
     update() {
+
         // gsap utils lerp function
         this.lerp.current = GSAP.utils.interpolate(
             this.lerp.current,
             this.lerp.target,
             this.lerp.ease
         )
-        // to animate camera alonge the curve on each frame
-        this.curve.getPointAt(this.lerp.current, this.position);
-        // this.progress -= 0.01;
 
+        this.curve.getPointAt(this.lerp.current % 1, this.position);
         this.camera.orthographicCamera.position.copy(this.position);
+
+        // // keeps moving when scroll ends
+        // if (this.back) {
+        //     this.lerp.target -= 0.001;
+        // } else {
+        //     this.lerp.target += 0.001;
+        // }
+        //
+        // // values in certain range
+        // this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
+        // this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
+        //
+        // // to animate camera alonge the curve on each frame
+        // this.curve.getPointAt(this.lerp.current, this.position);
+        //
+        // this.curve.getPointAt(this.lerp.current + 0.00001, this.lookAtPosition);
+        // // this.progress -= 0.01;
+        //
+        // this.camera.orthographicCamera.position.copy(this.position);
+        // this.camera.orthographicCamera.lookAt(this.lookAtPosition);
     }
 
 }
