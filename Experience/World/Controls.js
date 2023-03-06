4ce8d8fbae63d110b7424b9444d00896742a6420
gnulitskaya <webgnule@mail.ru>
2023-03-06 16:43:38 +0300

    responsive ScrollTrigger

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -7,6 +7,7 @@ export default class Controls {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
@@ -33,13 +34,16 @@ export default class Controls {
     setPath() {
         this.timeline = new GSAP.timeline();
         this.timeline.to(this.room.position, {
-            x: 0.5,
+            x: () => {
+                return this.sizes.width * 0.0012;
+            },
             scrollTrigger: {
                 trigger: ".first-move",
                 markers: true,
                 start: "top top",
                 end: "bottom bottom",
-                scrub: 0.6
+                scrub: 0.6,
+                invalidateOnRefresh: true
             }
         });
     }
