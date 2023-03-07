838bff6e8e944781029421ebab62aa74a48a08c9
gnulitskaya <webgnule@mail.ru>
2023-03-07 12:21:42 +0300

    edit theme toggle

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
 import GSAP from "gsap";
+import {RectAreaLightHelper} from "three/addons/helpers/RectAreaLightHelper.js";
 
 export default class Room {
     constructor() {
@@ -56,6 +57,20 @@ export default class Room {
                 });
             }
         });
+
+        // add light for Aquarium
+        const width = 0.5;
+        const height = 0.7;
+        const intensity = 1;
+        const rectLight = new THREE.RectAreaLight( 0xffffff, intensity,  width, height );
+        rectLight.position.set( 7.68244, 7, 0.5);
+        rectLight.rotation.x = -Math.PI / 2;
+        rectLight.rotation.z = -Math.PI / 4;
+
+        this.actualRoom.add( rectLight );
+
+        // const rectLightHelper = new RectAreaLightHelper( rectLight );
+        // rectLight.add( rectLightHelper );
 
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.23,0.23,0.23);
